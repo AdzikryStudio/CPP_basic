c22c3abd616684a592e47ee6eca6b046da913b5a
Faqih Adzikry <faqihbelajarid@gmail.com>
2024-12-31 08:21:08 +0700

    Add files via upload

---
+++ C++ KT/dowhileloop_pertemuan15/dwl.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	//do while loop 
+	// action -> require
+	
+	int a = 1;
+	
+	do { cout << "true" << endl;
+			a++;
+	}
+		while(a < 20);
+		
+		cout << "selesai" << endl;
+	return 0;
+}
